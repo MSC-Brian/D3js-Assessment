cdfd422e21f197a49b206d05d814458818ccbbd5
Brian Salant <brian.salant@mirlogic.com>
2019-09-09 11:46:51 -0400

    second commit

---
+++ static/js/app.js
@@ -0,0 +1,63 @@
+// Basic parameters of the svg
+var margin = {
+  top: 15,
+  bottom: 15,
+  left: 70,
+  right: 15
+};
+
+var width = 900;
+var height = 600;
+
+var svgHeight = height + margin.top + margin.bottom;
+var svgWidth = width + margin.left + margin.right;
+
+var svg = d3.select('#svg-area')
+            .append('svg')
+            .attr('width', svgWidth)
+            .attr('height', svgHeight);
+
+var tooltip = d3.select('body')
+                .append('div')
+                .attr('class', 'tooltip')
+                .attr('opacity', '0');
+
+// Read and arrange the data
+
+
+
+
+// Set scales
+
+
+
+
+// Create axes
+
+
+
+// Append the axes as G
+
+
+
+
+
+// Create Bars or Line function
+
+
+
+
+// Create Circle group
+
+
+
+
+// Mouseover / mouseout
+
+
+
+// tooltip
+
+
+
+// Legend function
